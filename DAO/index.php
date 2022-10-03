5d0069bfaed8128230c94545d16d8172dcc97002
OcordeiroBH <cordeiro.bruno1998@gmail.com>
2022-10-03 20:05:35 -0300

    Inclusao de estudo DAO pt2

--- DAO/index.php
+++ DAO/index.php
@@ -2,14 +2,41 @@
 
 require_once("config.php");
 
+/* 
 $sql = new Sql();
 
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
-echo json_encode($usuarios);
+echo json_encode($usuarios); */
 
+/*$root = new Usuario();
 
+carrega um unico usuario $root->loagById(1);
 
+echo $root; 
 
+*/
+
+/*$lista = Usuario::getList(); //podemos utilizar desta maneira pq getList é um metodo estatico
+
+carreg uma lista de usuarios
+
+echo json_encode($lista);
+*/
+
+/*
+
+Carrega o usuario com um filtro pelo login
+
+$search = Usuario::search("er");
+
+echo json_encode($search);
+
+*/
+
+$sql = new Sql();
+$usuario = new Usuario(); 
+$usuario->login("joao","Bruno123");
+echo $usuario;
 
 ?>
